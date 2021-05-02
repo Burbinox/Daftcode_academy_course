910e7e33d8535353587014de47ab748b6e00f81c
Burbinox <hubert.grobelny@interia.pl>
2021-05-02 19:50:35 +0200

    test

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI, HTTPException, Response
-import hashlib
+from fastapi import FastAPI, HTTPException, Response, Cookie
+import base64
+from typing import Optional
 import datetime
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
-app.secret_key = "bardzotrudnehaslodozlamaniaktorezawieraduzoznakowcyfr13412341spacji    iznakowspecjalnych#%^&$&$*%"
 app.access_tokens = []
 
 
@@ -15,21 +15,25 @@ def hello():
 
 
 @app.post("/login_session")
-def login_session(login: str, password: str, response: Response):
-    if login == "4dm1n" and password == "NotSoSecurePa$$":
-        session_token = hashlib.sha256(f"{login}{password}{app.secret_key}".encode()).hexdigest()
+def login(response: Response, user: Optional[str] = None,
+          password: Optional[str] = None):
+    if user == '4dm1n' and password == 'NotSoSecurePa$$':
+
+        session_token_uncode = f'{user}{password}dfsds'
+        session_token_bytes = session_token_uncode.encode('ascii')
+        base64_bytes = base64.b64encode(session_token_bytes)
+        session_token = base64_bytes.decode('ascii')
         app.access_tokens.append(session_token)
-        response.set_cookie(key="session_token", value="raz_dwa_trzyaaa")
+        response.set_cookie(key="session_token",
+                            value=session_token)
+        return {'token': session_token}
     else:
         raise HTTPException(status_code=401)
 
 
 @app.post("/login_token")
-def login_token(login: str, password: str, response: Response):
-    if login == "4dm1n" and password == "NotSoSecurePa$$":
-        session_token = hashlib.sha256(f"{login}{password}{app.secret_key}".encode()).hexdigest()
-        app.access_tokens.append(session_token)
-        response.set_cookie(key="session_token", value="raz_dwa_trzyaaa")
-        return {"token": session_token}
-    else:
+def create_cookie(*, response: Response, session_token: str = Cookie(None)):
+    if session_token not in app.access_tokens:
         raise HTTPException(status_code=401)
+    else:
+        return {'token': session_token}
